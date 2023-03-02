d81d8df158a9aa79d35183f28878a8f18c414efd
Marissa R.S <mrowles@academic.rrc.ca>
2023-03-02 13:17:24 -0600

    polished bullet

--- bullet.js
+++ bullet.js
@@ -1,11 +1,8 @@
 class Bullet extends Actor {
   constructor(position, velocity, color, ttl) {
-    super(position, 2);
+    super(position, 3);
 
     this.acceleration.set(velocity);
-
-    //this.angle = angle;
-    //this.isShot = false;
     
     this.color = color;
     this.ttl = ttl;
@@ -23,46 +20,9 @@ class Bullet extends Actor {
   draw(){
     push();
       fill(this.color);
-      circle(this.position.x, this.position.y, 10);
+      stroke(this.color);
+      strokeWeight(4);
+      point(this.position.x, this.position.y);
     pop();
-
   }
-
-  fire(){
-    let thrust = createVector(0, 1);
-    thrust.setHeading(this.angle - HALF_PI);
-    this.acceleration.add(thrust);
-
-    this.isShot = true;
-  }
-
-  updatePosition(x, y, angle) {
-    this.position.x = x;
-    this.position.y = y;
-    this.angle = angle;
-  }
-
-  display() {
-    
-
-    
-    if (this.isShot){
-      text(this.position, 10, 160);   
-      circle(this.position.x, this.position.y, 10);
-      let onEdge = this.isOnEdge(this.position);
-      if(onEdge){
-        this.isShot = false;
-      }
-    }
-    else{
-      this.angle-=HALF_PI
-      let a = 40 * cos(this.angle);
-      let b = 40 * sin(this.angle);
-  
-      circle(a + this.position.x, b + this.position.y, 10);
-      
-    }
-  }
-
-  
 }
--- bulletManager.js
+++ bulletManager.js
@@ -1,24 +1,42 @@
 class BulletManager {
-    constructor() {
-      this.bullets = [];
+  constructor() {
+    this.bullets = [];
+  }
+
+  addBullet(position, velocity, color, ttl) {
+    console.log("adding bullet");
+    this.bullets.push(new Bullet(position.copy(), velocity.copy(), color, ttl));
+  }
+
+  update() {
+    for (let bullet of this.bullets) {
+      bullet.update();
     }
 
-    addBullet(position, velocity, color, ttl){
-        console.log("adding bullet");
-        this.bullets.push(new Bullet(position.copy(), velocity.copy(), color, ttl));
-    }
+    this.bullets = this.bullets.filter((bullet) => bullet.alive());
+  }
 
-    update(){
-        for (let bullet of this.bullets) {
-            bullet.update();
-        }
-
-        this.bullets = this.bullets.filter(bullet => bullet.alive());
-    }
-
-    draw(){
-        for (const bullet of this.bullets) {
-            bullet.draw();
-        }
+  draw() {
+    for (const bullet of this.bullets) {
+      bullet.draw();
     }
   }
+
+  checkIfHitAsteroid(astroidManager, astroids) {
+    for (const bullet of this.bullets) {
+      for (const astroid of astroids) {
+        var d = dist(
+          bullet.position.x,
+          bullet.position.y,
+          astroid.position.y,
+          astroid.position.x
+        );
+
+        if (d < astroid.radius - 5) {
+          console.log("HIT");
+          astroid.break();
+        }
+      }
+    }
+  }
+}
