a2c6482c0339d3e68470201e3c07e3b73ffd016b
Marissa R.S <mrowles@academic.rrc.ca>
2023-03-02 15:06:11 -0600

    deleted redundant code

--- gameManager.js
+++ gameManager.js
@@ -2,17 +2,5 @@ class GameManager {
   constructor(startBtn) {
     this.mode = [0, 1, 2];
     this.activeMode = 0;
-
-    this.startBtn = startBtn;
-  }
-
-  draw() {
-    //console.log(this.activeMode);
-    if (this.activeMode == 0) {
-    }
-    if (this.activeMode == 1) {
-    }
-    if (this.activeMode == 2) {
-    }
   }
 }
